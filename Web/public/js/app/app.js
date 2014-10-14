69477aa5a1c6696404491d574adaa685f5e342ff
Mathieu le Tyrant <mathieu.letyrant@gmail.com>
2014-10-14 20:27:17 +0200

    Cosmetic and add readme for installation

--- Web/public/js/app/app.js
+++ Web/public/js/app/app.js
@@ -1,12 +1,12 @@
 angular.module('app', ['restangular', 'ui.router'])
 
     .constant('config', {
-        apiUrl: 'http://wayapi.mathieuletyrant.com',
-        token: 'KDR8u9vuRH8i6hx8V4e6'
+        apiUrl  : 'http://wayapi.mathieuletyrant.com',
+        token   : 'KDR8u9vuRH8i6hx8V4e6'
     })
     .config(function($stateProvider, $urlRouterProvider){
 
-        $urlRouterProvider.otherwise("/state1"); // Default Page
+        $urlRouterProvider.otherwise("/home"); // Default Page
 
         $stateProvider
             .state('home', {
