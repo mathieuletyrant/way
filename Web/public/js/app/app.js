b9ac037f395e3ff6dbabfbcaaab6c4c155017723
Mathieu le Tyrant <mathieu.letyrant@gmail.com>
2014-10-15 11:27:06 +0200

    Config facebook connect

--- Web/public/js/app/app.js
+++ Web/public/js/app/app.js
@@ -2,10 +2,18 @@
 
 angular.module('app', ['restangular', 'ui.router', 'facebook'])
 
+    /*
+        Config
+     */
     .constant('config', {
-        apiUrl  : 'http://wayapi.mathieuletyrant.com',
-        token   : 'KDR8u9vuRH8i6hx8V4e6'
+        apiUrl          : 'http://wayapi.mathieuletyrant.com',
+        token           : 'KDR8u9vuRH8i6hx8V4e6',
+        facebookAppApi  : '123456789' // TODO Need change when we have good key from Facebook :)
     })
+
+    /*
+        Route System
+     */
     .config(function($stateProvider, $urlRouterProvider){
 
         $urlRouterProvider.otherwise("/home"); // Default Page
@@ -15,4 +23,11 @@ angular.module('app', ['restangular', 'ui.router', 'facebook'])
                 url: "/home",
                 templateUrl: "templates/home.html"
             });
+    })
+
+    /*
+        Facebook Connect
+     */
+    .config(function(FacebookProvider, config) {
+        FacebookProvider.init(config.facebookAppApi);
     });
