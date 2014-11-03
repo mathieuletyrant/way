26a605e8e6c82934ffa79d1b4b7ae4d744e5cb05
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2014-11-03 13:47:25 +0100

    Fix Facebook connect

--- Web/public/js/app/controllers/authenticationController.js
+++ Web/public/js/app/controllers/authenticationController.js
@@ -5,39 +5,64 @@
  */
 angular.module('app').controller('authenticationController', function ($scope, Facebook) {
 
-    $scope.loginStatus = 'disconnected';
-    $scope.facebookIsReady = false;
-    $scope.user = null;
+    $scope.user = {};
+    $scope.logged = false;
 
-    $scope.login = function () {
-        Facebook.login(function (response) {
-            $scope.loginStatus = response.status;
-        });
-    };
-
-    $scope.removeAuth = function () {
-        Facebook.api({
-            method: 'Auth.revokeAuthorization'
-        }, function (response) {
-            Facebook.getLoginStatus(function (response) {
-                $scope.loginStatus = response.status;
-            });
-        });
-    };
-
-    $scope.api = function () {
-        Facebook.api('/me', function (response) {
-            $scope.user = response;
-        });
-    };
-
-    $scope.$watch(function () {
+    /*
+     * Watch for Facebook to be ready.
+     * There's also the event that could be used
+     */
+    $scope.$watch(
+        function() {
             return Facebook.isReady();
-        }, function (newVal) {
-            if (newVal) {
-                $scope.facebookIsReady = true;
-            }
+        },
+        function(newVal) {
+            if (newVal)
+                $scope.facebookReady = true;
         }
     );
 
+    var userIsConnected = false;
+
+    Facebook.getLoginStatus(function(response) {
+        if (response.status == 'connected') {
+            userIsConnected = true;
+        }
+    });
+
+    /*
+     * Login
+     */
+    $scope.login = function() {
+        Facebook.login(function(response) {
+            if (response.status == 'connected') {
+                $scope.logged = true;
+                $scope.me();
+            }
+        });
+    };
+
+    /*
+     * me
+     */
+    $scope.me = function() {
+        Facebook.api('/me', function(response) {
+            $scope.$apply(function() {
+                $scope.user = response;
+            });
+
+        });
+    };
+
+    /*
+     * Logout
+     */
+    $scope.logout = function() {
+        Facebook.logout(function() {
+            $scope.$apply(function() {
+                $scope.user   = {};
+                $scope.logged = false;
+            });
+        });
+    };
 });
