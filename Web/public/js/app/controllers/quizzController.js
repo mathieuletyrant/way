c4b74f2a14cd5f89c0bc0bf1495929548d4bf063
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2014-11-09 22:24:29 +0100

    Little fix last commit

--- Web/public/js/app/controllers/quizzController.js
+++ Web/public/js/app/controllers/quizzController.js
@@ -5,6 +5,16 @@ angular.module('app').controller('quizzController', function($scope, $stateParam
     var type = $stateParams.type || 'single';
 
     /*
+     * Load questions Single/Multi
+     */
+    quizz.loadQuestions(type).then(function(result){
+        $scope.questions = result.questions;
+        console.log(result.questions);
+    }, function(){
+        return 'Error during parse questions.';
+    });
+
+    /*
      * Quizz Start at Question 0 !
      */
     $scope.currentQuestion = 0;
@@ -20,16 +30,6 @@ angular.module('app').controller('quizzController', function($scope, $stateParam
     $scope.newQuestion = function(){
         $scope.responses[$scope.currentQuestion].value = Math.round(Math.random());
         $scope.currentQuestion++;
-    }
-
-    /*
-     * Load questions Single/Multi
-     */
-    quizz.loadQuestions(type).then(function(result){
-        $scope.questions = result.questions;
-        console.log(result.questions);
-    }, function(){
-        return 'Error during parse questions.';
-    });
+    };
 
 });
