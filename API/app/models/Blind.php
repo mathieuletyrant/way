aa4fa088bff3ba8b19eaeea5721a3675be8d1085
paulo17 <paulboiseau@gmail.com>
2014-11-12 12:18:37 +0100

    blind edit

--- API/app/models/Blind.php
+++ API/app/models/Blind.php
@@ -14,13 +14,25 @@ class Blind extends Model{
 		$insert = $this->db->prepare('INSERT INTO ' . $this->table . '(status, type, user_id, friend_id, created)
 			VALUES(:status, :type, :user_id, :friend_id, :created)');
 		$insert->execute(array(
-			'status' => $blind['status'],
+			'status' => 'CREATED',
 			'type' => $blind['type'],
 			'user_id' => $blind['user_id'],
 			'friend_id' => (!empty($blind['friend_id'])) ? $blind['friend_id'] : '';
 			'created' => $this->datetime()
 			));
 		return (!empty($insert)) ? $this->db->lastInsertId() : false;
+	}
+
+	public function validate($blind){
+		$validate = true;
+
+		if(empty($blind['type'])){ return false; }
+		if(empty($blind['user_id'])){ return false; }
+		if(isset($blind['friend_id'])){
+			if(empty($blind['friend_id'])) { return false; }
+		}
+
+		return $validate;
 	}
 
 }
