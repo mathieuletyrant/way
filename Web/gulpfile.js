37e2b3a6cf04d3331d316cd3a0d3afb7d78911f7
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2014-11-22 15:09:58 +0100

    Improve Gulp and Add NgAria

--- Web/gulpfile.js
+++ Web/gulpfile.js
@@ -13,10 +13,8 @@ var gulp        = require('gulp'),
         common : 'public'
     };
 
-gulp.task('default', ['clean'], function() {
-    gulp.start('script-app');
-    gulp.start('script-lib');
-    gulp.start('style');
+gulp.task('default', ['clean', 'script-app', 'script-lib', 'style'], function() {
+    gulp.start('watch');
 });
 
 /*
@@ -62,6 +60,7 @@ gulp.task('script-lib', function() {
     gulp.src([
         paths.common+'/js/libs/angular/angular.js',
         paths.common+'/js/libs/angular-animate/angular-animate.js',
+        paths.common+'/js/libs/angular-aria/angular-aria.js',
         paths.common+'/js/libs/angular-ui-router/release/angular-ui-router.js',
         paths.common+'/js/libs/angular-facebook/lib/angular-facebook.js',
         paths.common+'/js/libs/ngstorage/ngStorage.js'
